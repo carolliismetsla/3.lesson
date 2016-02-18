cf0ba148941108b24f7ada017c7563ce62e8655c
carmet <carmet@greeny.cs.tlu.ee>
2016-02-18 11:20:47 +0200

    added message to app

---
+++ app.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+
+ ?>
+
+ <h2> First application  </h2>
+
+<form> method="get">
+   <label for="message">Message:* <label><br>
+     <input type="text" name="message"><br>
+     <input type= "submit" value= "Save to DB">
+
+<form>
